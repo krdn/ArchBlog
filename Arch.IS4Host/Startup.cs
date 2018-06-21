5170c22151971baa6904d730103ad73da06090a5
김석곤 <krdn.net@gmail.com>
2018-06-21 15:42:02 +0900

    Add project files.

---
+++ Arch.IS4Host/Startup.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Arch.IS4Host.Data;
+using Arch.IS4Host.Models;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace Arch.IS4Host
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // 연결문자열을 var 변수에 저장함.
+            var connectionsString = Configuration.GetConnectionString("DefaultConnection");
+
+            // assambly for migration 저장
+            var migrationAddembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            // DbContext database SqlLite를 Postgres 교체
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(connectionsString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = false;
+            });
+
+            var builder = services.AddIdentityServer()
+                // User our Postgres Database for storing configuration data
+                .AddConfigurationStore(configDb => {
+                    configDb.ConfigureDbContext = db => db.UseNpgsql(connectionsString, 
+                    sql => sql.MigrationsAssembly(migrationAddembly));
+                })
+                
+                // User our Postgres Database for storing operational data
+                .AddOperationalStore(operationalDb => {
+                    operationalDb.ConfigureDbContext = db => db.UseNpgsql(connectionsString, 
+                    sql => sql.MigrationsAssembly(migrationAddembly));
+                })
+                .AddAspNetIdentity<ApplicationUser>();
+
+
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
+            // google, facebook, twitter 추가할수 있음
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
+                    options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            IniializeDatabase(app);
+            
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentityServer();
+            app.UseMvcWithDefaultRoute();
+        }
+
+        private void IniializeDatabase(IApplicationBuilder app) 
+        {
+            using(var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope()){
+
+                var persistedGrantDbContext = serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>();
+                persistedGrantDbContext.Database.Migrate();
+
+                var configDbContext = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                configDbContext.Database.Migrate();
+
+                if(!configDbContext.Clients.Any()) {
+                    foreach(var client in Config.GetClients()) {
+                        configDbContext.Clients.Add(client.ToEntity());
+                    }
+                    configDbContext.SaveChanges();
+                }
+
+                if(!configDbContext.IdentityResources.Any()) {
+                    foreach(var res in Config.GetIdentityResources()) {
+                        configDbContext.IdentityResources.Add(res.ToEntity());
+                    }
+                    configDbContext.SaveChanges();
+                }
+
+                if(!configDbContext.ApiResources.Any()) {
+                    foreach(var api in Config.GetApis()) {
+                        configDbContext.ApiResources.Add(api.ToEntity());
+                    }
+                    configDbContext.SaveChanges();
+                }                
+            }
+        }
+
+        
+    }
+}
